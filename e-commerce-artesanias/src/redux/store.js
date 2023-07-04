fdb9af69e72180c59479f06adb15b5ff8cb05a8a
Diego Samudio <dieguisam@gmail.com>
2023-07-03 22:34:50 -0500

    intall deps, create folders

---
+++ e-commerce-artesanias/src/redux/store.js
@@ -0,0 +1,10 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { useReducer } from "./userSlice";
+
+// export const store = configureStore({
+//     reducer: {
+//         user: useReducer,
+//     },
+// });
+
+//crear todos los slice necesarios 
---
+++ e-commerce-artesanias/src/redux/userSlice.js
@@ -0,0 +1,10 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//     name: "",
+//     username: "",
+//     email: "",
+
+// };
+
+//se continua creando el slice , recordar que se puede agilizar el proceso de creacion del slice usando otros slices
