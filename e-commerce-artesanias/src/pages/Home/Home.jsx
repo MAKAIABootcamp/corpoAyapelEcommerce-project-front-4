608a967eeb776b302235ccdd5924ba08bf1ce3f3
Diego Samudio <dieguisam@gmail.com>
2023-07-06 13:10:14 -0500

    adding products to home

--- e-commerce-artesanias/src/pages/Home/Home.jsx
+++ e-commerce-artesanias/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import NavBar from "../../components/Layout/NavBar/NavBar";
+import ProductCards from "../../components/Cards/ProductCards/ProductCards";
 
 const Home = () => {
   return <div>
     Home
     <NavBar/>
-
-
+    <ProductCards/>
   </div>;
 };
 
