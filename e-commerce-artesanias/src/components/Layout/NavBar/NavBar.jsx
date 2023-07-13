d06029d73074b262cedc5309182f8898be99ef19
Diego Samudio <dieguisam@gmail.com>
2023-07-13 00:05:42 -0500

    saving responsive navbar

--- e-commerce-artesanias/src/components/Layout/NavBar/NavBar.jsx
+++ e-commerce-artesanias/src/components/Layout/NavBar/NavBar.jsx
@@ -13,45 +13,43 @@ const NavBar = () => {
     <div>
       <nav className="nav-bar">
         <img src={logo} alt="logo" />
-        <ul>
+        <ul className="left-row">
           <li onClick={() => handleLinkClick("/")}>Artesanías</li>
           <li onClick={() => handleLinkClick("/AboutUs")}>¿Quiénes somos?</li>
           <li onClick={() => handleLinkClick("/Contact")}>Contacto</li>
-          <li onClick={() => handleLinkClick("/Search")}>Buscar</li>
         </ul>
 
-        <div className="validation">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="15%"
-            height="15%"
-            fill="currentColor"
-            className="bi bi-person-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-          </svg>
-          <ul>
-            {/* NO FUNCIONAN AUN */}
-            <li onClick={() => handleLinkClick("/Validation")}>Sign in</li>
-            <li onClick={() => handleLinkClick("/my-account")}>My account</li>
-          </ul>
-        </div>
+        <div className="right">
+          <div className="Search">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30"
+              height="30" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+          <p onClick={() => handleLinkClick("/Search")}>Buscar</p>
+          </div>
 
-        <div className="nav-bag">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="14%"
-            height="14%"
-            fill="currentColor"
-            className="bi bi-basket-fill"
-            viewBox="0 0 16 16"
-          >
-            <path d="M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717L5.07 1.243zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0v-3z" />
+          <div className="validation">
+          <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"/>
           </svg>
-          <span className="bag-quantity">
-            <span>3</span>
-          </span>
+            <ul>
+              {/* NO FUNCIONAN AUN */}
+              <li onClick={() => handleLinkClick("/Validation")}>Sign in</li>
+              {/* SI ESTA LOGUEADO APARECE EL MY ACCOUNT */}
+              <li onClick={() => handleLinkClick("/my-account")}>My account</li>
+            </ul>
+          </div>
+
+          <div className="nav-bag">
+          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+          </svg>
+            <ul className="cart-value">
+              <li>My cart</li>
+              <li>$177.000</li>
+            </ul>
+          </div>
+
         </div>
       </nav>
     </div>
