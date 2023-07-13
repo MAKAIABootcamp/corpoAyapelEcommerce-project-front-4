9cf6c8d48f343dca521ba76883804b5058f65ac5
Diego Samudio <dieguisam@gmail.com>
2023-07-12 22:36:20 -0500

    hot fix

--- e-commerce-artesanias/src/components/Layout/NavBar/NavBar.jsx
+++ e-commerce-artesanias/src/components/Layout/NavBar/NavBar.jsx
@@ -32,6 +32,7 @@ const NavBar = () => {
             <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
           </svg>
           <ul>
+            {/* NO FUNCIONAN AUN */}
             <li onClick={() => handleLinkClick("/Validation")}>Sign in</li>
             <li onClick={() => handleLinkClick("/my-account")}>My account</li>
           </ul>
