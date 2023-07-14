c0bf1a124ec6fc82a0518234a0ac97210c756579
natalia maldonado <108nrm@gmail.com>
2023-07-13 21:01:09 -0500

    button login

--- e-commerce-artesanias/src/redux/reducers/loginReducer.js
+++ e-commerce-artesanias/src/redux/reducers/loginReducer.js
@@ -37,6 +37,7 @@ export const loginReducer = (state = initialState, action ) =>{
             return {
                 ...state,
                 ...action.payload,
+                
             };
         case loginTypes.LOGOUT_USER:
             return{
