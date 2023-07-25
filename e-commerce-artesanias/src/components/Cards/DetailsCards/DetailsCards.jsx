b8d16c2d0593590d813bd7d3d8618f4f032bfea3
Paola <paolandrea96@gmail.com>
2023-07-25 12:17:42 -0500

    Cart with storage

--- e-commerce-artesanias/src/components/Cards/DetailsCards/DetailsCards.jsx
+++ e-commerce-artesanias/src/components/Cards/DetailsCards/DetailsCards.jsx
@@ -77,55 +77,56 @@ const DetailsCards = () => {
 
   const onAddingToCart = (productId) => {
     // Aquí añadimos la lógica para agregar el producto al carrito con su cantidad
-  if (productInfo) {
-    // Comprobamos si el carrito ya existe en el local storage
-    const existingCart = localStorage.getItem("productsInCart");
-    let cartItems = existingCart ? JSON.parse(existingCart) : {};
+    if (productInfo) {
+      // Comprobamos si el carrito ya existe en el local storage
+      const existingCart = localStorage.getItem("productsInCart");
+      let cartItems = existingCart ? JSON.parse(existingCart) : {};
+      const newProductId = productInfo._id;
+      if (cartItems[newProductId]) {
+        // Si el producto ya está en el carrito, aumentamos la cantidad en 1
+        cartItems[newProductId].quantity += 1;
+      } else {
+        // Si el producto no está en el carrito, lo agregamos con cantidad 1
+        cartItems[newProductId] = {
+          ...productInfo,
+          quantity: 1,
+        };
+      }
 
-    const newProductId = productInfo._id;
-    if (cartItems[newProductId]) {
-      // Si el producto ya está en el carrito, aumentamos la cantidad en 1
-      cartItems[newProductId].quantity += 1;
-    } else {
-      // Si el producto no está en el carrito, lo agregamos con cantidad 1
-      cartItems[newProductId] = {
-        ...productInfo,
-        quantity: 1,
-      };
+      // Guardamos el carrito actualizado en el local storage
+      localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+
+      // Mostramos un mensaje de éxito usando el estado setShowToast
+      setShowToast(true);
     }
-
-    // Guardamos el carrito actualizado en el local storage
-    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-
-    // Mostramos un mensaje de éxito usando el estado setShowToast
-    setShowToast(true);
-  }
-};
+  };
   return (
     <div>
       {productInfo ? (
-        <div className="containerDetails">
-          <div className="detailsImages">
-            <img
-              className="mainImage"
-              // src={selectedImage || product.image1?.asset?._ref}
-              // src={urlFor(productInfo[`image${1}`].asset._ref).url()}
-              src={selectedImage ? urlFor(selectedImage).url() : urlFor(productInfo[`image${1}`].asset._ref).url()}
-              alt="Product main Image" />
-            <div className="containerOtherImages" >
-              {Array.from({ length: 4 }, (_, index) => (
-                productInfo[`image${index + 1}`]?.asset?._ref && (
-                  <img
-                    key={`image-${index}`}
-                    className={`otherimg ${selectedImage === productInfo[`image${index + 1}`]?.asset?._ref ? 'active' : ''}`}
-                    variant="top"
-                    src={urlFor(productInfo[`image${index + 1}`].asset._ref).url()}
-                    alt={productInfo.name}
-                    onClick={() => setSelectedImage(productInfo[`image${index + 1}`]?.asset?._ref)}
-                  />
-                )
-              ))}
-              {/* {productInfo.img && Object.values(productInfo.img).map((imageUrl, index) => (
+        <div className="containerAllDetails">
+          <h1 className="tittleProductName">{productInfo?.name}</h1>
+          <div className="containerDetails">
+            <div className="detailsImages">
+              <img
+                className="mainImage"
+                // src={selectedImage || product.image1?.asset?._ref}
+                // src={urlFor(productInfo[`image${1}`].asset._ref).url()}
+                src={selectedImage ? urlFor(selectedImage).url() : urlFor(productInfo[`image${1}`].asset._ref).url()}
+                alt="Product main Image" />
+              <div className="containerOtherImages" >
+                {Array.from({ length: 4 }, (_, index) => (
+                  productInfo[`image${index + 1}`]?.asset?._ref && (
+                    <img
+                      key={`image-${index}`}
+                      className={`otherimg ${selectedImage === productInfo[`image${index + 1}`]?.asset?._ref ? 'active' : ''}`}
+                      variant="top"
+                      src={urlFor(productInfo[`image${index + 1}`].asset._ref).url()}
+                      alt={productInfo.name}
+                      onClick={() => setSelectedImage(productInfo[`image${index + 1}`]?.asset?._ref)}
+                    />
+                  )
+                ))}
+                {/* {productInfo.img && Object.values(productInfo.img).map((imageUrl, index) => (
                 <img
                   key={index}
                   className={`otherimg ${imageUrl === selectedImage ? 'active' : ''}`}
@@ -137,40 +138,44 @@ const DetailsCards = () => {
 
 
 
+              </div>
+
             </div>
+            <div className="detailsInfo">
 
-          </div>
-          <div className="detailsInfo">
-            <h1 className="tittleProductName">{productInfo?.name}</h1>
-            <h3 className="subtittleProductPrice"> {numberToMoney(productInfo?.Precio)} </h3>
-            <p className="productDescription">{productInfo?.description2} </p>
-            <h4 className="subtittleProductDescription"> Cuidados y recomendaciones </h4>
-            <p className="productDescription">{productInfo?.recomendations} </p>
+              <p className="productDescription">{productInfo?.description} </p>
+              <p className="productDescription">{productInfo?.description2} </p>
+              <hr/>
+              <h4 className="subtittleProductDescription"> Cuidados y recomendaciones </h4>
+              <p className="productDescription">{productInfo?.recomendations} </p>
+              <hr/>
+              <h3 className="subtittleProductPrice"> Valor por unidad: {numberToMoney(productInfo?.Precio)} </h3>
 
-            <Button className="button" onClick={() => {
-              onAddingToCart(product._id);
-              toggleToast();
-            }}>
-              Agregar al carrito
-            </Button>
-            <Toast className="toast"
-              show={showToast}
-              onClose={toggleToast}
-              autohide
-              delay={5000}
-            >
-              <Toast.Header>
-                <strong className="me-auto">Producto agregado</strong>
-              </Toast.Header>
-              <Toast.Body>¡El producto se ha agregado con éxito!</Toast.Body>
-            </Toast>
+              <Button className="button" onClick={() => {
+                onAddingToCart(product._id);
+                toggleToast();
+              }}>
+                Agregar al carrito
+              </Button>
+              <Toast className="toast"
+                show={showToast}
+                onClose={toggleToast}
+                autohide
+                delay={5000}
+              >
+                <Toast.Header>
+                  <strong className="me-auto">Producto agregado</strong>
+                </Toast.Header>
+                <Toast.Body>¡El producto se ha agregado con éxito!</Toast.Body>
+              </Toast>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>Producto no encontrado</div>
+          </div>
+            ) : (
+            <div>Producto no encontrado</div>
       )}
-    </div>
-  );
+          </div>
+          );
 };
 
-export default DetailsCards;
+          export default DetailsCards;
