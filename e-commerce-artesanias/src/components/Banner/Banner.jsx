b9451161136d41322643d0108129023d6a8765dd
Diego Samudio <dieguisam@gmail.com>
2023-07-24 20:23:06 -0500

    fixing banners

--- e-commerce-artesanias/src/components/Banner/Banner.jsx
+++ e-commerce-artesanias/src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import { getAllBanners } from '../../../Services/servicesSanity';
+import { urlFor } from '../../../sanityClient';
 import "./Banner.scss";
 
 const Banner = () => {
@@ -7,15 +8,17 @@ const Banner = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
-    // Obtiene los datos de los banners desde el endpoint
-    axios
-      .get("http://localhost:3000/banner")
-      .then((response) => {
-        setBanners(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    // Fetch all banners from Sanity when the component mounts
+    const fetchBanners = async () => {
+      try {
+        const bannersData = await getAllBanners();
+        setBanners(bannersData);
+      } catch (error) {
+        console.error('Error fetching banners:', error);
+      }
+    };
+
+    fetchBanners();
   }, []);
 
   const handlePrevSlide = () => {
@@ -33,19 +36,19 @@ const Banner = () => {
       {banners.length > 0 && (
         <div className="banner">
           <button className="prev-button" onClick={handlePrevSlide}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-              <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
             </svg>
           </button>
           <img
             className="slide"
-            src={banners[currentSlide].imageUrl}
+            src={urlFor(banners[currentSlide].imageUrl).url()}
             alt={banners[currentSlide].title}
           />
           <button className="next-button" onClick={handleNextSlide}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-          </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg>
           </button>
         </div>
       )}
