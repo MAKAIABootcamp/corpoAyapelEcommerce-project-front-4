f4d6d24efa1bd25a493fa092074ff76b7c51f481
Diego Samudio <dieguisam@gmail.com>
2023-07-26 06:43:21 -0500

    fixing additional

--- e-commerce-artesanias/src/components/Layout/NavBar/NavBar.jsx
+++ e-commerce-artesanias/src/components/Layout/NavBar/NavBar.jsx
@@ -30,49 +30,17 @@ const NavBar = () => {
             <li onClick={() => handleLinkClick('/Contact')}>Contacto</li>
           </ul>
 
-          <div className='Search'>
-            <svg
+          <div className='Search'><svg
               xmlns='http://www.w3.org/2000/svg'
-              width='30'
-              height='30'
+              width='28'
+              height='28'
               fill='currentColor'
               className='bi bi-search'
               viewBox='0 0 16 16'
-            >
-              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-            </svg>
+            ><path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' /></svg>
             <p onClick={() => handleLinkClick('/Search')}>Buscar</p>
-            {/* <ul> */}
-              {/* NO FUNCIONAN AUN */}
-              {/* {user.name !== '' ? (
-                <li>{user.name}</li>
-              ) : (
-                <li onClick={() => handleLinkClick('/Login')}>
-                  Iniciar sesion
-                </li>
-              )} */}
-              {/* SI NO ESTA LOGUEADO APARECE EL MI CUENTA Y CUANDO ESTA LOGUEADO APARECE EL NOMBRE Y SE QUITA LA OPCION DE INICIAR SESION */}
-              {/* <li onClick={() => handleLinkClick('/MyAccount')}>Mi cuenta</li>
-            </ul> */}
-          </div>
 
-          {/* <div className='nav-bag'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='27'
-              height='27'
-              fill='currentColor'
-              className='bi bi-cart'
-              viewBox='0 0 16 16'
-            >
-              <path d='M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z' />
-            </svg>
-            <ul className='cart-value'>
-              <li>Mi carrito</li>
-              <li>MIRAR COMENTARIO</li> */}
-              {/* SE NECESITA VINCULACION CON EL TOTAL DEL CARRITO */}
-            {/* </ul>
-          </div> */}
+          </div>
 
           <div className='validation'>
             <svg
@@ -112,7 +80,6 @@ const NavBar = () => {
             </svg>
             <ul className='cart-value'>
               <li onClick={() => handleLinkClick('/Cart')}>Mi carrito</li>
-              <li></li>
               {/* SE NECESITA VINCULACION CON EL TOTAL DEL CARRITO */}
             </ul>
           </div>
@@ -145,10 +112,6 @@ const NavBar = () => {
               </li>
               <li onClick={() => handleLinkClick('/Contact')}>Contacto</li>
             </ul>
-
-            <div className='Search-mobile'>
-              <p onClick={() => handleLinkClick('/Search')}>Buscar</p>
-            </div>
 
             <div className='Search-mobile'>
               <p onClick={() => handleLinkClick('/Search')}>Buscar</p>
